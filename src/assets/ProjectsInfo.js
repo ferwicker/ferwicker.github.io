96aa4fe9a2116a11dfb4dbb412072fdd008d6dd1
Fer Wicker <fer.wicker@gmail.com>
2021-04-25 21:19:13 +1000

    create project components

---
+++ src/assets/ProjectsInfo.js
@@ -0,0 +1,19 @@
+const ProjectsInfo = [
+{
+    name: 'teamspark',
+    title: 'TeamSpark⚡',
+    deployed: 'https://ferwicker.com/team-spark/',
+    repo: 'https://github.com/ferwicker/team-spark',
+    screenshot: '',
+    screenshot2: '',
+    technologies: ['React', 'Javascript', 'Bootstrap'],
+    intro: 'TeamSpark is an employee directory management app that helps organisations with a team spread accross multiple locations access their employees contact details easily.',
+    description: `<p>With workplaces changing rapidly and more people working from home or remotely than ever before, organisations need a way to easily access everyone's contact details to make day to day communication easier. TeamSpark⚡ aims to facilitate this with a central company directory that everyone in the organisation can access.</p>
+        <p>This app displays the team list on a table with the main fields always visible, view each person's full details, search employees by first name, last name or city, filter the table by state and sort by either first or last name.</p>
+        <p>Future updates for this application include user logins, adding your own database of employees and having more useful fields to search and filter from.</p>`,
+    featured: true
+},
+
+];
+
+export default ProjectsInfo;
