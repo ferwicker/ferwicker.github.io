90cefe77add402e5a263e1408dcb821b2bafa09b
Fer Wicker <fer.wicker@gmail.com>
2021-04-25 18:33:52 +1000

    Nav and Footer components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import "./style.css";
+import logo from '../../assets/images/ferwicker-logo-white.png'
 
 function Footer () {
     return (
-        <footer>
+        <footer className='fixed-bottom' style={{zIndex:-1}}>
+            <div className='container d-flex flex-column align-items-center'>
+                <Link to="/">
+                    <img className="footer-brand" src={logo} alt='Fer Wicker logo'></img>
+                </Link>
 
+                    <div className="d-flex flex-row align-items-center">
+                        <a aria-hidden="true" className="fas fa-file-download fa-2x social-icon" title="Download resume" href="" target="_blank"><span class="sr-only">Download Resume</span></a>
+                        <a aria-hidden="true" className="fab fa-github fa-2x social-icon" title="Github" href="https://github.com/ferwicker" target="_blank"><span class="sr-only">Github profile</span></a>
+                        <a aria-hidden="true" className="fab fa-linkedin-in fa-2x social-icon" title="Linkedin" href="https://www.linkedin.com/in/ferwicker/" target="_blank"><span class="sr-only">Linkedin profile</span></a>
+                        <a aria-hidden="true" className="fab fa-instagram fa-2x social-icon" title="Instagram" href="https://www.instagram.com/fer.wicker/" target="_blank"><span class="sr-only">Instagram profile</span></a>
+                    </div>
+                    <Link to='/contact'className='footer-btn'>
+                        Contact me
+                    </Link>
+            </div>
         </footer>
     )
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import "./style.css";
+import logo from '../../assets/images/ferwicker-logo.png'
 
 function Nav () {
     return (
-        <nav>
-
+        <nav className='navbar navbar-expand-lg fixed-top'>
+            <div className='container'>
+                <Link to="/">
+                    <img className="navbar-brand" src={logo} alt='Fer Wicker logo'></img>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="fas fa-ellipsis-h"></span>
+                </button>
+                <div className='collapse navbar-collapse justify-content-end' id="navbarNavAltMarkup">
+                    <Link to="/" className='nav-link nav-text'>
+                        Home
+                    </Link>
+                    <Link to="/about" className='nav-link nav-text'>
+                        About
+                    </Link>
+                    <Link to="/portfolio" className='nav-link nav-text'>
+                        Portfolio
+                    </Link>
+                    <Link to="/contact" className='nav-link nav-text'>
+                        Contact
+                    </Link>
+                    <div className='d-flex'>
+                    <a className="nav-link nav-icon fab fa-github fa-2x" href="https://github.com/ferwicker" target="_blank">
+                    </a>
+                    <a className="nav-link nav-icon fas fa-file-download fa-2x" href="" target="_blank">
+                    </a>
+                    </div>
+                </div>
+            </div>
         </nav>
     )
 }
