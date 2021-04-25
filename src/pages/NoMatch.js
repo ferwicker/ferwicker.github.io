30dbc87e54faab0d5af706915a2d1322ab16bdbe
Fer Wicker <fer.wicker@gmail.com>
2021-04-26 06:33:45 +1000

    No Match page

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,13 +1,18 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 function NoMatch() {
 
     //logic goes here
 
     return (
-        <div>
-            404 page
-        </div>
+        <section className='home-section container d-flex flex-column justify-content-middle align-items-center '>
+            <h1 className='h1-404'>404</h1>
+            <p className='p-404'>Whoops! Looks like the page you are looking for is not here!</p>
+            <Link to='/' role='button' className='main-btn'>
+                Return to homepage
+            </Link>
+        </section>
     );
 }
 
