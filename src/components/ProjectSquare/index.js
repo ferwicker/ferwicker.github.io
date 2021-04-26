c225f4d09acd535c7b2fe57223bf070a8fecc116
Fer Wicker <fer.wicker@gmail.com>
2021-04-26 20:32:16 +1000

    project preview components

--- src/components/ProjectSquare/index.js
+++ src/components/ProjectSquare/index.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 import "./style.css";
 //import round from '../../assets/images/project-round.png'
 
-function ProjectRound (props) {
+function ProjectSquare (props) {
     const projectLink = `/projects/${props.name}`
     return (
         <Link to={projectLink} key='props.name' className='project-link'>
-            <div className='project-preview d-flex justify-content-center align-items-center' >
+            <div className='project-preview-sq d-flex justify-content-center align-items-center' >
                 <h3 className='project-name'>{props.title}</h3>
             </div>
         </Link>
     );
 };
 
-export default ProjectRound;
+export default ProjectSquare;
