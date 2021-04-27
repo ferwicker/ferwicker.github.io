5bc10c113bb0d657c366fac753a9190ddbbc47c0
Fer Wicker <fer.wicker@gmail.com>
2021-04-27 21:35:21 +1000

    higher res logo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./style.css";
-import logo from '../../assets/images/ferwicker-logo.png'
+import logo from '../../assets/images/ferwicker-logo.svg'
 
 function Nav () {
     return (
