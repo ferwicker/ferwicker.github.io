04590b6958c6deca004a8da47df1a0b1b5fe7f00
Fer Wicker <fer.wicker@gmail.com>
2021-04-27 20:14:37 +1000

    change class to className on fontawesome icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,10 +12,10 @@ function Footer () {
                 </Link>
 
                     <div className="d-flex flex-row align-items-center">
-                        <a aria-hidden="true" className="fas fa-file-download fa-2x social-icon" title="Download resume" href="" target="_blank"><span class="sr-only">Download Resume</span></a>
-                        <a aria-hidden="true" className="fab fa-github fa-2x social-icon" title="Github" href="https://github.com/ferwicker" target="_blank"><span class="sr-only">Github profile</span></a>
-                        <a aria-hidden="true" className="fab fa-linkedin-in fa-2x social-icon" title="Linkedin" href="https://www.linkedin.com/in/ferwicker/" target="_blank"><span class="sr-only">Linkedin profile</span></a>
-                        <a aria-hidden="true" className="fab fa-instagram fa-2x social-icon" title="Instagram" href="https://www.instagram.com/fer.wicker/" target="_blank"><span class="sr-only">Instagram profile</span></a>
+                        <a aria-hidden="true" className="fas fa-file-download fa-2x social-icon" title="Download resume" href="" target="_blank"><span className="sr-only">Download Resume</span></a>
+                        <a aria-hidden="true" className="fab fa-github fa-2x social-icon" title="Github" href="https://github.com/ferwicker" target="_blank"><span className="sr-only">Github profile</span></a>
+                        <a aria-hidden="true" className="fab fa-linkedin-in fa-2x social-icon" title="Linkedin" href="https://www.linkedin.com/in/ferwicker/" target="_blank"><span className="sr-only">Linkedin profile</span></a>
+                        <a aria-hidden="true" className="fab fa-instagram fa-2x social-icon" title="Instagram" href="https://www.instagram.com/fer.wicker/" target="_blank"><span className="sr-only">Instagram profile</span></a>
                     </div>
                     <Link to='/contact'className='footer-btn'>
                         Contact me
